062c11d889ceb9dc89a6074b68f91c83c957f9fe
Matt Krueger <mkrueger@backwoodsguru.com>
2013-05-31 01:41:12 -0500

    changed to support query of multiple symbols in one request refactored StockQuoter to eliminate duplicated code changed default URL to static local variable from app.config file added test code to command line test for multiple symbol query

--- YahooFinance.NET/StockQuoter.cs
+++ YahooFinance.NET/StockQuoter.cs
@@ -12,43 +12,45 @@ namespace YahooFinance.NET
 {
     public class StockQuoter
     {
-        /// <summary>
-        /// This original method grabs the YQL url from the config file, but it was causing problems int testing
-        /// so i created the second method where the url is manually passed in.
-        /// </summary>
-        /// <param name="ticker"></param>
-        /// <returns></returns>
+        private string yahoo_url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22||TICKER||%22)%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env";
+
         public yQuote StockQuote(string ticker)
         {
-            yQuote qte = new yQuote();
-            string requesturl = ConfigurationManager.AppSettings["URL"];
-            requesturl = requesturl.Replace("||TICKER||", ticker);
+            return StockQuote(ticker, yahoo_url);
+        }
 
-            // MAKE WEB REQUEST TO THE URL
-            WebRequest request = WebRequest.Create(requesturl);
-            WebResponse response = request.GetResponse();
-
-            Stream receive = response.GetResponseStream();
-            Encoding encoding = Encoding.GetEncoding("utf-8");
-            StreamReader read = new StreamReader(receive, encoding);
-
-            string content = read.ReadToEnd();
-
-            // DESERIALIZE THE JSON RESPONSE TO AN OBJECT
-            JObject yahooresult = JObject.Parse(content);
-            JToken result = yahooresult["query"]["results"]["quote"];
-            qte = JsonConvert.DeserializeObject<yQuote>(result.ToString());
-
-            return qte;
+        public List<yQuote> StockQuote(List<string> tickers)
+        {
+            return StockQuote(tickers, yahoo_url);
         }
 
         public yQuote StockQuote(string ticker, string url)
         {
-
-
             yQuote qte = new yQuote();
-            string requesturl = url;
-            requesturl = requesturl.Replace("||TICKER||", ticker);
+            JToken result = yahoo_query(url, ticker);
+            qte = JsonConvert.DeserializeObject<yQuote>(result.ToString());
+            return qte;
+        }
+
+        public List<yQuote> StockQuote(List<string> tickers, string url)
+        {
+            yQuote qte = new yQuote();
+            List<yQuote> quotes = new List<yQuote>();
+            string ticker_request="";
+            foreach( string symbol in tickers)
+            {
+                ticker_request += symbol;
+                ticker_request += ",";
+            }
+            ticker_request = ticker_request.TrimEnd(';');
+            JToken result = yahoo_query(url, ticker_request);
+            quotes = JsonConvert.DeserializeObject<List<yQuote>>(result.ToString());
+            return quotes;
+        }
+
+        private JToken yahoo_query(string url, string ticker_request)
+        {
+            string requesturl = url.Replace("||TICKER||", ticker_request);
 
             // MAKE WEB REQUEST TO THE URL
             WebRequest request = WebRequest.Create(requesturl);
@@ -63,10 +65,8 @@ namespace YahooFinance.NET
             // DESERIALIZE THE JSON RESPONSE TO AN OBJECT
             JObject yahooresult = JObject.Parse(content);
             JToken result = yahooresult["query"]["results"]["quote"];
-            qte = JsonConvert.DeserializeObject<yQuote>(result.ToString());
 
-
-            return qte;
+            return result;
         }
     }
 }
--- yfnquote/Program.cs
+++ yfnquote/Program.cs
@@ -13,21 +13,40 @@ namespace yfntest
         static void Main(string[] args)
         {
             System.Console.WriteLine("yahoo finance stock quote test");
-            String url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22||TICKER||%22)%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env";
 
             StockQuoter quoter = new StockQuoter();
-            yQuote quote = quoter.StockQuote("MSFT", url);
+            yQuote quote = quoter.StockQuote("MSFT");
+            WriteQuote(quote);
+            System.Console.WriteLine();
 
+            List<string> symbols = new List<string>();
+            symbols.Add("IBM");
+            symbols.Add("GOOG");
+            symbols.Add("AAPL");
+            symbols.Add("FNORD");
+
+            List<yQuote> quotes = quoter.StockQuote(symbols);
+
+            foreach (yQuote q in quotes)
+            {
+                WriteQuote(q);
+                Console.WriteLine();
+            }
+
+            System.Console.WriteLine();
+            System.Console.WriteLine("[Hit Enter to close...]");
+            System.Console.ReadLine();
+        }
+
+        static void WriteQuote(yQuote quote)
+        {
             Console.WriteLine("symbol={0}", quote.symbol);
+            Console.WriteLine("name={0}", quote.Name);
             Console.WriteLine("bid={0}", quote.BidRealtime);
             Console.WriteLine("ask={0}", quote.AskRealtime);
             Console.WriteLine("last={0}", quote.LastTradePriceOnly);
             Console.WriteLine("time={0}", quote.LastTradeTime);
             Console.WriteLine("volume={0}", quote.Volume);
-
-            System.Console.WriteLine();
-            System.Console.WriteLine("[Hit Enter to close...]");
-            System.Console.ReadLine();
         }
     }
 }
